725e0c8121fd3cbe2c03852ee101f4f3145db4f8
Robat Williams <robatwilliams@gmail.com>
2019-10-11 21:02:53 +0100

    Sweeping blink effect

---
+++ blinkstick-strip/blink-sweep/script.js
@@ -0,0 +1,107 @@
+import { OFF, ORANGE } from '../colors.js';
+
+// These IDs appear to be shared by all BlinkStick products.
+// This demo is for the 8-LED Strip (https://www.blinkstick.com/products/blinkstick-strip)
+// https://github.com/arvydas/blinkstick-node/blob/master/blinkstick.js#L22
+const vendorId = 0x20a0;
+const productId = 0x41e5;
+
+const LED_COUNT = 8;
+
+let running = false;
+
+document.querySelector('#start').addEventListener('click', handleStart);
+document.querySelector('#stop').addEventListener('click', () => (running = false));
+
+async function handleStart() {
+  running = true;
+
+  const device = await getOpenedDevice();
+
+  await clear(device);
+
+  const effectGenerator = blinkSweep();
+
+  while (running) {
+    const action = effectGenerator.next().value;
+
+    if (typeof action === 'number') {
+      await wait(action);
+    } else if (action.length === 2) {
+      const [index, color] = action;
+      setColor(device, index, color);
+    } else {
+      const color = action;
+
+      for (let i = 0; i < LED_COUNT; i++) {
+        await setColor(device, i, color);
+      }
+    }
+  }
+
+  await clear(device);
+}
+
+async function getOpenedDevice() {
+  const devices = await navigator.hid.getDevices();
+  let device = devices.find(d => d.vendorId === vendorId && d.productId === productId);
+
+  if (!device) {
+    device = await navigator.hid.requestDevice({
+      filters: [{ vendorId, productId }],
+    });
+  }
+
+  if (!device.opened) {
+    await device.open();
+  }
+
+  return device;
+}
+
+async function setColor(device, index, [r, g, b], retries = 1) {
+  // Limit the brightness (still bright!); at some higher level it starts getting stuck on (overheating?)
+  r *= 0.5;
+  g *= 0.5;
+  b *= 0.5;
+
+  // Info gleaned from https://github.com/arvydas/blinkstick-node/blob/master/blinkstick.js#L429
+  const reportId = 5;
+  const data = Int8Array.from([reportId, index, r, g, b]);
+
+  try {
+    await device.sendFeatureReport(reportId, data);
+  } catch (error) {
+    if (retries > 0) {
+      await setColor(device, index, [r, g, b], --retries);
+    } else {
+      console.error(`Failed to set color at index ${index}`, error);
+    }
+  }
+}
+
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function clear(device) {
+  for (let i = 0; i < LED_COUNT; i++) {
+    await setColor(device, i, [0, 0, 0]);
+  }
+}
+
+function* blinkSweep() {
+  // https://www.youtube.com/watch?v=_mqiQ3vILD8
+  const SWEEP_DURATION = 250;
+  const OFF_DURATION = 700;
+
+  while (true) {
+    for (let i = 0; i < LED_COUNT; i++) {
+      yield [i, ORANGE];
+      yield SWEEP_DURATION / LED_COUNT;
+    }
+
+    yield OFF;
+    yield OFF_DURATION;
+  }
+}
